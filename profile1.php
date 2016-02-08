968a35af75d300188a54c26a0b332c8f3f5090b9
it5706021620016 <jopt_1996@hotmail.com>
2016-02-09 00:41:52 +0700

    5706021620016

--- profile1.php
+++ profile1.php
@@ -1,4 +1,3 @@
-<!DOCTYPE html>
 <html>
   <head>
     <meta charset="utf-8">
@@ -6,49 +5,62 @@
   </head>
   <body align="center">
     <?php
-    $name = $_GET['name'];
-    $lname =$_GET['lname'];
-    $id = $_GET['id'];
-    $edu = $_GET['edu'];
-    $nname = $_GET['nname'];
-    $sex = $_GET['sex'];
-    $email = $_GET['email'];
-    $age = $_GET['age'];
-    $phone = $_GET['phone'];
-    $hobby = $_GET['hobby'];
+    $prefix = $_POST['prefix'];
+    $name = $_POST['name'];
+    $lname =$_POST['lname'];
+    $id = $_POST['id'];
+    $edu = $_POST['edu'];
+    $nname = $_POST['nname'];
+    $sex = $_POST['sex'];
+    $email = $_POST['email'];
+    $age = $_POST['age'];
+    $phone = $_POST['phone'];
+    $hobby = $_POST['hobby'];
+    $bdate = $_POST['bdate'];
+    $bmonth = $_POST['bmonth'];
+    $byear = $_POST['byear'];
+    $since = $byear-543;
+    $mailtype =$_POST['mailtype'];
 
 
 
-    echo "<p>";
-    echo "<b>ข้อมูลที่กรอกมา</b><br/>";
-    echo "ชื่อ-นามสกุล : $name ";
-    echo " $lname </br>";
+    echo "<big>|| ข้อมูลที่ผู้ใช้กรอกมา ||</big><br><br>";
+    echo "$prefix $name $lname </br>";
+    echo "ชื่อเล่น : $nname </br>";
     echo "รหัสนักศึกษา : $id </br>";
     echo "ระดับการศึกษา : $edu </br>";
-    echo "ชื่อเล่น : $nname </br>";
     echo "เพศ : $sex </br>";
-    echo "อีเมลล์ : $email </br>";
 
-    if($age > 0 && $age<60){
-      echo "อายุ : $age  ปี </br>";
-    }elseif ($age>=60 && $age<=100) {
+    echo "เกิดวันที่ $bdate เดือน  $bmonth พ.ศ. $byear (ค.ศ. $since) <br>";
+  //  if($age > 0 && $age<60){
+  //    echo "อายุ : $age  ปี </br>";
+  //  }
+    if ($age >=6 && $age<=12) {
+      echo "อายุ : $age ปี (ประถมศึกษา)  <br>";
+    }
+    elseif ($age >=13 && $age <=15) {
+      echo "อายุ : $age ปี (มัธยมศึกษาตอนต้น)  <br>";
+    }
+    elseif ($age >=16 && $age <=18) {
+      echo "อายุ : $age ปี (มัธยมศึกษาตอนปลาย)  <br>";
+    }
+    elseif ($age >=19 && $age <=22) {
+      echo "อายุ : $age ปี (อุดมศึกษา)  <br>";
+    }
+
+    elseif ($age>=60 && $age<=100) {
       echo "อายุ : เกษียณ </br>";
     }
-      else{
-      echo "อายุ : คุณกรอกอายุผิดพลาด</br>";
-
+      elseif ($age<0 && $age>100){
+     echo "อายุ : คุณกรอกอายุผิดพลาด</br>";
+  }
+    else{
+      echo " อายุ : <br>" ;
     }
-
+    echo "อีเมลล์ : $email";
+    echo "$mailtype </br>";
     echo "เบอร์โทรศัพท์ : $phone </br>";
     echo "งานอดิเรก : $hobby </br></br></br>";
-
-
-
-
-
-
-
-
 
      ?>
   </body>
